4e8e5478b8e03a3081a33eb1a2ae729d6d7efad4
Nurullah Torun <mntorun@protonmail.com>
2023-08-04 23:39:56 +0300

    some fixes and company section done

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,44 @@
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope) {
 
+    $scope.companies = [
+        {
+            id:1,
+            name:'As Adana',
+            logo:'/src/img/companies/asadana.png'
+        },
+        {
+            id:2,
+            name:'As Tur',
+            logo:'/src/img/companies/as.png'
+        },
+        {
+            id:3,
+            name:'Ulusoy',
+            logo:'/src/img/companies/ulu.png'
+        },
+        {
+            id:4,
+            name:'Rota',
+            logo:'/src/img/companies/rota.png'
+        },
+        {
+            id:5,
+            name:'Nevsehir',
+            logo:'/src/img/companies/new.png'
+        },
+        {
+            id:6,
+            name:'Kamil Koc',
+            logo:'/src/img/companies/kamil.png'
+        },
+        {
+            id:7,
+            name:'Efe Tur',
+            logo:'/src/img/companies/efe.png'
+        },
+        
+    ]
+
+
 });
